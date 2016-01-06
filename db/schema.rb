4b1438bce56fe22204707f4036c7d214a9034dbb
Luiz Fernando A. Vasconcellos <luiz_vasconcellos@hotmail.com>
2016-01-06 16:00:30 -0200

    generates tag model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160104193808) do
+ActiveRecord::Schema.define(version: 20160106175956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,4 +36,14 @@ ActiveRecord::Schema.define(version: 20160104193808) do
     t.string   "github_url"
   end
 
+  create_table "tags", force: :cascade do |t|
+    t.string   "content"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "tags", ["post_id"], name: "index_tags_on_post_id", using: :btree
+
+  add_foreign_key "tags", "posts"
 end
