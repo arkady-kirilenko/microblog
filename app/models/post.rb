6b72368077c33ee6db7f3d96d98e48a784ecdcff
Luiz Fernando A. Vasconcellos <luiz_vasconcellos@hotmail.com>
2016-01-14 02:51:33 -0200

    post model changes to slug

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,14 @@
 class Post < ActiveRecord::Base
+  extend FriendlyId
+  friendly_id :title, use: [:slugged, :finders
+  ]
   has_many :taggings, dependent: :destroy
   has_many :tags, through: :taggings
   has_many :comments, :as => :commentable
+
+  def slug_candidates
+    [
+      :name,
+      [:name, :created_at]
+    ]
 end
