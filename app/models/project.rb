50fc5969b90ec97b8643ca3afb76f684b48a6f72
Luiz Fernando A. Vasconcellos <luiz_vasconcellos@hotmail.com>
2016-01-19 16:22:49 -0200

    image uploading resize

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, use: [:slugged, :finders]
 
-  has_attached_file :banner, styles: { banner:"1920x300>", medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :banner, styles: { banner:"1920x300>", show:"1024x300#" }, default_url: "/images/:style/missing.png"
 
   validates_attachment_content_type :banner, content_type: /\Aimage\/.*\Z/
   validates :title, presence: true, length: { minimum: 3, maximum: 144}, uniqueness: true
